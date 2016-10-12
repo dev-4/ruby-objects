ff4f9bbd5c9e35c862fff7edcb622c6454712cc0
Dev <dev-4@users.noreply.github.com>
2016-10-12 16:37:55 -0400

    Update lift.rb

--- lift.rb
+++ lift.rb
@@ -1,17 +1,18 @@
+# The tower that contains the lift
 class Tower
   def lift
     puts "You are on floor " + rand(12).to_s + " "
   end
 end
-
+# New lift
   my_Tower = Tower.new
   my_Tower.lift
   
   puts "What floor do you want?"
   input = gets.chomp.to_i
-  
+  # Lift can only go to 1-12
   floors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-  
+  # If and else
   if input < 1 || input > 12
     puts "The is no floor #{input}, sorry."
   else
